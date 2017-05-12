9c22383a61a9155db97f222623357d87fc5c47b9
Larsson Omberg <lom@larssono.com>
2017-05-12 10:37:50 -0700

    Add package install information

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='synapsebridgehelpers',
+      version='0.1',
+      description='Convenience functions and code for dealing with bridge data in Synapse',
+      url='https://github.com/Sage-Bionetworks/synapsebridgehelpers',
+      author='Larson Omberg and Yooree Chae',
+      author_email='platform@sagebase.org',
+      license='Apache',
+      packages=['synapsebridgehelpers'],
+      install_requires=[
+          'synapseclient',
+          'pandas'],
+      zip_safe=False)
