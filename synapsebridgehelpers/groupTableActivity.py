468920e5a2b241e1c3dffcab9ba5dd8c4b4e0b98
Meghasyam Tummalacherla <itismeghasyam@gmail.com>
2017-08-16 16:15:26 -0700

    Update groupTableActivity.py
    Had an issue with grouping by externalId, now it works perfectly

--- synapsebridgehelpers/groupTableActivity.py
+++ synapsebridgehelpers/groupTableActivity.py
@@ -1,8 +1,9 @@
 import synapsebridgehelpers
        
     
-def groupTableActivity(syn,sourceProjId):
-    """ Groups all table versions according to corresponding activities """
+def groupTableActivity(syn,sourceProjId, extIdStr = ''):
+    """ Groups all table versions according to corresponding activities, 
+    has option to find tables with certain externalIds """
 
     # this function checks if the string is named like this '********-v*', 
     # and returns the check(True if -v** is version) and the position of the '-v' in the string
@@ -46,7 +47,26 @@ def groupTableActivity(syn,sourceProjId):
         else:
             table_activities[element] = [nameIdMap[element]]
     
+    if extIdStr == '':
+        return table_activities
+    else:
+        temp_table_activities = {}           
+        for element in table_activities:
+            table_ids_activity = table_activities[element]
+            for table_id in table_ids_activity:
+                results = syn.tableQuery('select * from '+ table_id +' where externalId like "' + extIdStr + '%"')
+                df = results.asDataFrame()
+                if df.shape[0]*df.shape[1] > 0:
+                    if element in temp_table_activities:
+                        temp_table_activities[element].append(table_id)
+                    else:
+                        temp_table_activities[element] = [table_id]
+                    
+        table_activities = temp_table_activities
+        
     return table_activities
+
+
     
-    
+
 
