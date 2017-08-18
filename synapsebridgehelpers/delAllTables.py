a77f56463e0d9912bf20307919871921b5f6cba4
Meghasyam Tummalacherla <itismeghasyam@gmail.com>
2017-08-18 12:06:34 -0700

    Update delAllTables.py
    Added comments and changed names of variables for easier read

--- synapsebridgehelpers/delAllTables.py
+++ synapsebridgehelpers/delAllTables.py
@@ -5,16 +5,16 @@ def delAllTables(syn,projectId):
     all_tables = syn.getChildren(projectId,includeTypes=[u'table'],sortBy=u'NAME', sortDirection=u'ASC')
     df_table = pd.DataFrame(all_tables)
     
+    # Checking to see if given project folder is empty 
     if df_table.shape[0]*df_table.shape[1] == 0:
         print('No tables in the given project : ' + str(projectId))
     else:
-    
-        # find all unique table names in the list of tables, they will be named like parkinson-*****-v*
-        #df_un = pd.unique(df_table['name'])
+        # find all unique table ids in the project folder
         df_un_id = pd.unique(df_table['id'])
-
-        for i in df_un_id:
-            schema = syn.get(str(i))
+        
+        # iterating to delete each table id
+        for table_id in df_un_id:
+            schema = syn.get(str(table_id))
             syn.delete(schema)
         print('Done deleting all tables in the given project : ' + str(projectId))
 
