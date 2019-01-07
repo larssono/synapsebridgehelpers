990899a2405719f18b1791fe8cdfd6456c7498aa
Larsson Omberg <lom@larssono.com>
2019-01-07 15:34:34 -0800

    Remove deprecated synapse APIs.

--- synapsebridgehelpers/summaryTable.py
+++ synapsebridgehelpers/summaryTable.py
@@ -2,10 +2,12 @@ import synapsebridgehelpers
 import synapseclient
 import pandas as pd
 
-DEFAULT_SUMMARY_COLUMNS = ['recordId', 'appVersion','phoneInfo','uploadDate','healthCode','externalId',
-                           'dataGroups','createdOn','createdOnTimeZone','userSharingScope']
+DEFAULT_SUMMARY_COLUMNS = ['recordId', 'appVersion', 'phoneInfo', 'uploadDate', 'healthCode',
+                           'externalId', 'dataGroups', 'createdOn', 'createdOnTimeZone',
+                           'userSharingScope']
 
-def summarizeTables(syn, projectId, columns = DEFAULT_SUMMARY_COLUMNS):
+
+def summarizeTables(syn, projectId, columns=DEFAULT_SUMMARY_COLUMNS):
     """Summarizes all tables in a project by fetching the same columns from each table
     and concatenating the rows into a dataframe.
 
@@ -18,15 +20,15 @@ def summarizeTables(syn, projectId, columns = DEFAULT_SUMMARY_COLUMNS):
 
     all_tables = synapsebridgehelpers.get_tables(syn, projectId)
     df_list = []
-    for table_id in all_tables['table.id']:
-        table_columns =[str(col.name) for col in syn.getTableColumns(table_id)]
+    for table_id in all_tables['id']:
+        table_columns = [str(col.name) for col in syn.getTableColumns(table_id)]
         available_columns = list(set(columns).intersection(table_columns))
         columns_str = ','.join(available_columns)
-        df = syn.tableQuery('select %s from %s' %(columns_str,table_id))
+        df = syn.tableQuery('select %s from %s' % (columns_str, table_id))
         df = df.asDataFrame()
         schema = syn.get(table_id)
-        df['originalTableName'] = [schema.name for count in range(0,df.shape[0])]
-        df['originalTableId'] = [schema.id for count in range(0,df.shape[0])]
+        df['originalTableName'] = [schema.name for count in range(0, df.shape[0])]
+        df['originalTableId'] = [schema.id for count in range(0, df.shape[0])]
         df_list.append(df)
-    df_main = pd.concat(df_list)    
+    df_main = pd.concat(df_list)
     return df_main
--- synapsebridgehelpers/tableHelpers.py
+++ synapsebridgehelpers/tableHelpers.py
@@ -4,46 +4,48 @@ from synapseclient.exceptions  import SynapseHTTPError
 
 
 def get_tables(syn, projectId, simpleNameFilters=[]):
-    """Returns all the tables in a projects as a dataFrame with 
+    """Returns all the tables in a projects as a dataFrame with
     columns for synapseId, table names, Version and Simplified Name
-    
+
      Arguments:
     - syn: a Synapse client object
     - projectId: Synapse ID of the project we want tables from
-    - simpleNameFilters: the strings that are to be 
+    - simpleNameFilters: the strings that are to be
     filtered out from the table names to create a simple name"""
-    
-    tables = syn.chunkedQuery('select name,id from table where projectId=="%s"' %projectId)
+
+    tables = syn.getChildren(projectId, includeTypes=['table'])
     tables = pd.DataFrame(list(tables))
     # removing tables named 'parkinson-status' and 'parkinson-appVersion'
-    tables = tables[(tables['table.name']!='parkinson-status') &(tables['table.name']!='parkinson-appVersion')]
-    tables['version'] = tables['table.name'].str.extract('(.)(-v\d+)', expand=True)[1]
-    names = tables['table.name'].str.extract('([ -_a-z-A-Z\d]+)(-v\d+)',expand=True)[0]
+    tables = tables[(tables['name']!='parkinson-status') & (tables['name']!='parkinson-appVersion')]
+    tables['version'] = tables['name'].str.extract('(.)(-v\d+)', expand=True)[1]
+    names = tables['name'].str.extract('([ -_a-z-A-Z\d]+)(-v\d+)', expand=True)[0]
     print(names)
     for word in simpleNameFilters:
-        names = [name.replace(word,'') for name in names]
-    tables['simpleName'] = names     
-    return tables 
+        names = [name.replace(word, '') for name in names]
+    tables['simpleName'] = names
+    return tables
+
 
 def find_tables_with_data(syn, tables, healthCodes):
     """Go through a list of tables and find those where there is data given a
     data frame with sought healthCodes.
 
-    Returns the tables data frame with an additional column containing the 
+    Returns the tables data frame with an additional column containing the
     number of unique healthCodes found in each table."""
-    query = ("select count(distinct healthCode) from %s where healthCode in ('"+
-            "','".join(healthCodes)+"')")
-    counts = [syn.tableQuery(query %synId, resultsAs='rowset').asInteger() for
-              synId in tables['table.id']]
+    query = ("select count(distinct healthCode) from %s where healthCode in ('" +
+             "','".join(healthCodes) + "')")
+    counts = [syn.tableQuery(query % synId, resultsAs='rowset').asInteger() for
+              synId in tables['id']]
     tables['healthCodeCounts'] = counts
     return tables
 
+
 def query_across_tables(syn, tables, query, continueOnMissingColumn=True):
     """Runs a query across a list of tables and returns a list of results
-    
+
     param syn: a synapse object obtained through a syn.login()
     param tables: a list of synapse ids
-    param query: a query with a unassigne string in the from clause. E.g. "select foo from %s" 
+    param query: a query with a unassigne string in the from clause. E.g. "select foo from %s"
 
     """
     def safeQuery(query):
@@ -51,9 +53,9 @@ def query_across_tables(syn, tables, query, continueOnMissingColumn=True):
             return syn.tableQuery(query)
         except SynapseHTTPError as err:
             if err.response.status_code == 400 and continueOnMissingColumn:
-                return 
+                return
             else:
                 raise err
 
     mp = Pool(8)
-    return mp.map(lambda synId: safeQuery(query %synId), tables)
+    return mp.map(lambda synId: safeQuery(query % synId), tables)
